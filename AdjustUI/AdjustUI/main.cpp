ed18d2e9d0c7d0450b6b9b5908adacafc75608dd
v012345 <v138468@gmail.com>
2023-01-30 14:26:58 +0800

    update AdjustUI

--- AdjustUI/AdjustUI/main.cpp
+++ AdjustUI/AdjustUI/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "pugixml.hpp"
+
+void traverse();
+
 int main(int argc, char const *argv[])
 {
     std::cout << "start adjusting ui" << std::endl;
@@ -15,4 +18,27 @@ int main(int argc, char const *argv[])
     std::cout << doc.child("GameFile").child("Content").child("Content").attribute("ctype").value() << std::endl;
     std::cout << "end adjusting ui" << std::endl;
     return 0;
+}
+
+void traverse()
+{
+    for (pugi::xml_node tool = tools.child("Tool"); tool; tool = tool.next_sibling("Tool"))
+    {
+        std::cout << "Tool " << tool.attribute("Filename").value();
+        std::cout << ": AllowRemote " << tool.attribute("AllowRemote").as_bool();
+        std::cout << ", Timeout " << tool.attribute("Timeout").as_int();
+        std::cout << ", Description '" << tool.child_value("Description") << "'\n";
+    }
+
+    for (pugi::xml_node_iterator it = tools.begin(); it != tools.end(); ++it)
+    {
+        std::cout << "Tool:";
+
+        for (pugi::xml_attribute_iterator ait = it->attributes_begin(); ait != it->attributes_end(); ++ait)
+        {
+            std::cout << " " << ait->name() << "=" << ait->value();
+        }
+
+        std::cout << std::endl;
+    }
 }
